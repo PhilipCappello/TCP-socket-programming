77dbe1c692f953f5c440d207afcb220e7b556b3c
PhilipCappello <pcappello.2000@gmail.com>
2021-11-27 13:13:06 -0500

    upload program that gets web address from user, create a TCP connection and exchange data between client and server

---
+++ 216465098.py
@@ -0,0 +1,29 @@
+# first the client and server need to handshake and establish tcp connection 
+# when creating TCP connection we associate it with the client socket address (IP address and port number)
+#   -> find out how to get client/host socket address, do same for www.google.com (or the web address user enters)
+# when client creates its TCP socket, it specifies the address of the welcoming socket in the server (IP address of server host and port number of the socket)
+# after this, client then initiates three-way handshake and etablishes TCP connection with the server 
+
+# serverSocket is the socket created by the server that is dedicated to the client specifically (initial point of contact for all clients wanting to 
+#                                                                                                communicate with the server) 
+# connectionSocket is the newly creted socket dedicated to the client making the connection (each connection socket is subsequently created for communicating 
+#                                                                                               with each client)
+
+import socket 
+webAddress = input('Enter a web address and press Enter: ')
+serverName = socket.gethostbyname(webAddress)
+# print('IP address of '+webAddress+' is '+serverName)
+serverPort = 80
+# create client socket (param1: using IPv$; param2: socket type TCP)
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = (serverName,serverPort)
+clientSocket.connect((serverName,serverPort))
+request = "GET / HTTP/1.1\r\nHost:%s\r\n\r\n" % webAddress
+clientSocket.send(request.encode())
+print(clientSocket.recv(4096))
+# clientSocket.recv(1024)
+# sentence = input('Input lowercase sentence:')
+# clientSocket.send(sentence.encode())
+# modifiedSentence = clientSocket.recv(1024)
+# print('From Server: ', modifiedSentence.decode())
+# clientSocket.close()
